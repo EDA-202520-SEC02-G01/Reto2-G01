c9358b4b0e6c6deeadc4a22b5e889c2acf55d87e
Mateo-Avila <m.avilab2@uniandes.edu.co>
2025-10-17 22:01:02 -0500

    fix8

--- App/logic.py
+++ App/logic.py
@@ -204,6 +204,8 @@ def req_2(catalog, inicio, final, N):
     res=sl.quick_sort(res,sl.sort_criteriar2) #uso quick sort con un sort criteria personalizado para este requerimiento
     s=sl.size(res)
     respuesta=[]
+    primeros = []
+    ultimos = []
     if s > 2*N:
         primeros=res[:N] 
         ultimos=res[-N:]
